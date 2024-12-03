8967391ae2bf62fa985edb8749628a8cd93765c8
Nana Mohamadi <adilnana78@gmail.com>
2024-12-03 22:24:42 +0100

    Update and delete documents(personal and date)

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,17 +3,21 @@
 import React from "react";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./templates-gallery";
-import { useQuery } from "convex/react";
+import { usePaginatedQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
+import { DocumentsTable } from "./documents-table";
 
 const Home = () => {
-  const documents = useQuery(api.documents.get)
+  // Get all information of document
+  // const documents = useQuery(api.documents.get)
 
-  if(documents === undefined){
-    return (
-      <p>Loading...</p>
-    )
-  }
+  // if(documents === undefined){
+  //   return (
+  //     <p>Loading...</p>
+  //   )
+  // }
+
+  const {results, status, loadMore} = usePaginatedQuery(api.documents.get, {}, { initialNumItems: 5 })
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -22,9 +26,14 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery/>
-        {documents?.map((document) =>(
+        {/* {documents?.map((document) =>(
           <span key={document._id}>{document.title}</span>
-        ))}
+        ))} */}
+        <DocumentsTable 
+               documents={results}
+               loadMore={loadMore}
+               status={status}
+        />
       </div>
     </div>
   );
