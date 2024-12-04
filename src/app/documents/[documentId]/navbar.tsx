b78091b6e6857966303c789c6166e0bae574101c
Nana Mohamadi <adilnana78@gmail.com>
2024-12-04 23:14:49 +0100

    Populate user information

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -36,6 +36,7 @@ import {
 import { BsFilePdf } from "react-icons/bs";
 import { useEditorState } from "@/app/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Avatars } from "./avatars";
 
 export const Navbar = () => {
   const { editor } = useEditorState();
@@ -259,6 +260,7 @@ export const Navbar = () => {
         </div>
       </div>
       <div className="flex gap-3 items-center pl-6">
+        <Avatars />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/"
           afterLeaveOrganizationUrl="/"
